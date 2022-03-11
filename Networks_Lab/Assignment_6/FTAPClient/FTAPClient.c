0d12bdb555b330ccdcd48c14cbf6f2a054d83653
Ph4t3 <nikhilr.v2.0@gmail.com>
2022-03-12 01:08:24 +0530

    Added Networks Lab Assgn 6

---
+++ Networks_Lab/Assignment_6/FTAPClient/FTAPClient.c
@@ -0,0 +1,146 @@
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <stdbool.h>
+
+#define MAX 100
+#define PORT 1235
+#define SIZE 500
+
+typedef struct Packet {
+    int size;
+    int seq_no;
+    int ack_no;
+    char data[SIZE];
+} Packet;
+
+Packet* packet;
+int curr_file_size = 0;
+FILE* graph_ptr;
+
+int connect_socket()
+{
+    int sockfd, connfd, ret;
+    struct sockaddr_in servaddr, cli;
+
+    // socket create and verification
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd == -1) {
+        printf("ERR: Socket creation failed.\n");
+        exit(0);
+    }
+
+    // assign IP, PORT
+    bzero(&servaddr, sizeof(servaddr));
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    servaddr.sin_port = htons(PORT);
+
+    // connect the client socket to server socket
+    ret = connect(sockfd, (struct sockaddr*)&servaddr, sizeof(servaddr));
+    if (ret != 0) {
+        printf("Connection Failed.\n");
+        return -1;
+    }
+
+    printf("200: OK Connection is set up\n");
+    return sockfd;
+}
+
+void* timer_thread()
+{
+    int prev_file_size = 0;
+    float elapsed_time = 0;
+    while (1) {
+        int speed = (curr_file_size - prev_file_size) * 10; // In bytes per second
+        prev_file_size = curr_file_size;
+        elapsed_time += 0.1; // In Seconds
+        fflush(stdout);
+        printf("\rTransmission rate = %d KB/s ", speed / 1024);
+        fprintf(graph_ptr, "%f\t%d\n", elapsed_time, speed / 1024);
+        usleep(100000);
+    }
+}
+
+void send_file(int sockfd) {
+
+}
+
+void recv_file(int sockfd)
+{
+    int recv_size, curr_seq_no = 0, flag = 0;
+    FILE* fp;
+    graph_ptr = fopen("stats.dat", "w");
+    fp = fopen("outfile", "w");
+
+    pthread_t timer_t;
+    pthread_create(&timer_t, NULL, timer_thread, NULL);
+
+    while (!flag) {
+        memset(packet, 0, sizeof(Packet));
+        recv_size = recv(sockfd, packet, sizeof(Packet), 0);
+
+        if (recv_size > 0 && packet->seq_no == curr_seq_no) {
+            printf("%s\n", packet->data);
+            fwrite(packet->data, sizeof(char), packet->size, fp);
+            curr_file_size += packet->size;
+            curr_seq_no++;
+
+            if (packet->size < SIZE) {
+                pthread_cancel(timer_t);
+                flag = 1;
+            }
+        }
+
+        memset(packet, 0, sizeof(Packet));
+        packet->ack_no = curr_seq_no - 1;
+
+        if (send(sockfd, packet, sizeof(*packet), 0) == -1) {
+            perror("[-]Error in sending file.");
+            exit(1);
+        }
+    }
+
+    fclose(fp);
+}
+
+int main()
+{
+    int sockfd = -1, n;
+    packet = malloc(sizeof(Packet));
+
+    while(1) {
+        n = 0;
+        printf("> ");
+        memset(packet, 0, sizeof(Packet));
+        while ((packet->data[n++] = getchar()) != '\n');
+        packet->data[n - 1] = '\0';
+
+        if(sockfd == -1) {
+            if(strcmp("START", packet->data) == 0)
+                sockfd = connect_socket();
+            else
+                printf("Please establish a connection first.\n");
+        } else if (strcmp("QUIT", packet->data) == 0) {
+            close(sockfd);
+            sockfd = -1;
+        } else {
+            send(sockfd, packet, sizeof(Packet), 0);
+            memset(packet, 0, sizeof(Packet));
+
+            if(strncmp("StoreFile", packet->data, 9) == 0) {
+                send_file(sockfd);
+            } else if(strncmp("GetFile", packet->data, 7) == 0) {
+                recv_file(sockfd);
+            } else {
+                recv(sockfd, packet, sizeof(Packet), 0);
+                printf("%s\n", packet->data);
+            }
+        }
+    }
+}
---
+++ Networks_Lab/Assignment_6/FTAPServer/FTAPServer.c
@@ -0,0 +1,288 @@
+#include <arpa/inet.h>
+#include <errno.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <time.h>
+#include <unistd.h>
+#include <dirent.h>
+
+#define MAX 100
+#define PORT 1235
+#define BACKLOG 5
+#define SIZE 500
+
+typedef struct Packet {
+    int size;
+    int seq_no;
+    int ack_no;
+    char data[SIZE];
+} Packet;
+Packet* packet;
+
+typedef struct Users {
+    char username[100];
+    char password[100];
+    struct Users *nextUser;
+} Users;
+Users *users;
+
+void getUsers() {
+    users = NULL;
+    FILE *fp;
+    fp = fopen("../logincred.txt", "r");
+
+    char str[100];
+    while (fgets(str, sizeof(str), fp)) {
+        Users *user = malloc(sizeof(Users));
+        strcpy(user->username, strtok(str, ", \n"));
+        strcpy(user->password, strtok(NULL, ", \n"));
+        user->nextUser = users;
+        users = user;
+    }
+}
+
+Users* findUser(char* username) {
+    Users *head = users;
+    while(head != NULL) {
+        if(strcmp(username, head->username) == 0)
+            return head;
+        head = head->nextUser;
+    }
+    return NULL;
+}
+
+char* randstring(size_t sizegth)
+{
+    static char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789,.-#'?!";
+    char* randomString = NULL;
+
+    if (sizegth) {
+        randomString = malloc(sizeof(char) * (sizegth + 1));
+
+        if (randomString) {
+            for (int n = 0; n < sizegth; n++) {
+                int key = rand() % (int)(sizeof(charset) - 1);
+                randomString[n] = charset[key];
+            }
+            randomString[sizegth] = '\0';
+        }
+    }
+
+    return randomString;
+}
+
+void send_file(int sockfd)
+{
+    int count, recv_size;
+    FILE* fp = fopen("myfile.bin", "rb");
+    int curr_seq_no = 0;
+
+    while (1) {
+        memset(packet, 0, sizeof(Packet));
+        count = fread(packet->data, sizeof(char), SIZE, fp);
+        packet->seq_no = curr_seq_no;
+        packet->size = count;
+
+        if (count == 0)
+            return;
+
+        clock_t begin = clock();
+        if (send(sockfd, packet, sizeof(*packet), 0) == -1) {
+            perror("[-]Error in sending file.");
+            exit(1);
+        }
+
+        memset(packet, 0, sizeof(Packet));
+        recv_size = recv(sockfd, packet, sizeof(Packet), 0);
+
+        clock_t end = clock();
+        double RTT = (double)(end - begin) / CLOCKS_PER_SEC;
+        if (recv_size == -1 || packet->ack_no != curr_seq_no) {
+            perror("[-]Error: ACK Invalid");
+            fseek(fp, -SIZE, SEEK_CUR);
+        } else {
+            printf("\rRound Trip Time (RTT) = %lfs", RTT);
+            fflush(stdout);
+            curr_seq_no++;
+        }
+    }
+}
+
+char* list_files() {
+    DIR *dp;
+    struct dirent *ep;
+    char *out = malloc(0);
+    dp = opendir ("./");
+
+    if (dp != NULL) {
+        while ((ep = readdir (dp)) != NULL) {
+            out = realloc(out, strlen(out) + strlen(ep->d_name) + 1);
+            sprintf(out + strlen(out), "%s\n", ep->d_name);
+        }
+
+        out[strlen(out) - 1] = '\0';
+        closedir (dp);
+    } else
+        perror ("Couldn't open the directory");
+
+    return out;
+}
+
+// Main func for FTP protocol
+void ftp(int connfd)
+{
+    int count, index, ret, authenticated = 0;
+    Users* user = NULL;
+    char cmd[20], arg[100], *temp;
+    packet = malloc(sizeof(Packet));
+
+    // infinite loop for chat
+    while (1) {
+        memset(packet, 0, sizeof(Packet));
+        memset(cmd, 0, 20);
+        memset(arg, 0, 100);
+
+        if(!recv(connfd, packet, sizeof(Packet), 0)) // Read message from client
+            return;
+
+        strcpy(cmd,strtok(packet->data, " \n"));
+        temp = strtok(NULL, " \n");
+        if(temp != NULL)
+            strcpy(arg, temp);
+
+        memset(packet, 0, sizeof(Packet));
+        printf("Cmd: %s, Argument: %s\n", cmd, arg);
+
+        if(authenticated) {
+            if (!strcmp(packet->data, "GivemeyourVideo")) {
+                send_file(connfd);
+                printf("\nFile sent successfully...\n");
+            } else if (strcmp(cmd, "ListDir") == 0) {
+                strcpy(packet->data, list_files());
+                send(connfd, packet, sizeof(Packet), 0);
+                break;
+            } else {
+                strcpy(packet->data, randstring(20));
+                send(connfd, packet, sizeof(Packet), 0);
+            }
+        } else {
+            if (strcmp("USERN", cmd) == 0) {
+                user = findUser(arg);
+
+                if(user == NULL)
+                    strcpy(packet->data, "301: Incorrect Username");
+                else
+                    strcpy(packet->data, "300: Correct Username; Need password");
+            } else if(user != NULL && strcmp("PASSWD", cmd) == 0){
+                if(strcmp(user->password, arg) == 0) {
+                    authenticated = 1;
+                    strcpy(packet->data, "305: User Authenticated with password");
+                } else
+                    strcpy(packet->data, "310: Incorrect password");
+            } else {
+                strcpy(packet->data, "505: Command not supported");
+            }
+
+            send(connfd, packet, sizeof(Packet), 0);
+        }
+    }
+}
+
+void sigchld_handler(int s)
+{
+    // waitpid() might overwrite errno, so we save and restore it
+    int saved_errno = errno;
+    while (waitpid(-1, NULL, WNOHANG) > 0)
+        ;
+    errno = saved_errno;
+}
+
+void reaper()
+{
+    struct sigaction sa;
+    sa.sa_handler = sigchld_handler;
+    sigemptyset(&sa.sa_mask);
+    sa.sa_flags = SA_RESTART;
+    if (sigaction(SIGCHLD, &sa, NULL) == -1) {
+        perror("sigaction");
+        exit(1);
+    }
+}
+
+int init_socket()
+{
+    int sockfd, ret;
+    struct sockaddr_in serv_addr;
+
+    // Create Socket
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd == -1) {
+        printf("ERR: Socket creation failed.\n");
+        exit(0);
+    }
+
+    // Assign IP, PORT
+    bzero(&serv_addr, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    serv_addr.sin_port = htons(PORT);
+
+    // Bind Socket
+    ret = bind(sockfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr));
+    if (ret != 0) {
+        printf("ERR: Socket bind failed.\n");
+        exit(0);
+    }
+
+    // Listen for connection
+    if ((listen(sockfd, BACKLOG)) != 0) {
+        printf("ERR: Listen failed.\n");
+        exit(0);
+    }
+
+    return sockfd;
+}
+
+// Driver function
+int main()
+{
+    int connfd, sockfd;
+    struct sockaddr_in their_addr;
+    socklen_t addr_size;
+
+    // Initialize Socket
+    sockfd = init_socket();
+
+    // Reap all dead processes
+    reaper();
+
+    // Load the users file
+    getUsers();
+
+    while (1) {
+        // Accept the data packet from client and verification
+        addr_size = sizeof(their_addr);
+        connfd = accept(sockfd, (struct sockaddr*)&their_addr, &addr_size);
+        if (connfd < 0) {
+            printf("ERR: Server accept failed.\n");
+            exit(0);
+        }
+
+        if (!fork()) {     // Child process
+            close(sockfd); // child doesn't need the listener
+            ftp(connfd);
+            close(connfd);
+            exit(0);
+        }
+
+        close(connfd);
+    }
+}
+
